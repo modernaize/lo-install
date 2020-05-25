0e02501c0cc4cf37d221c552136db785a0ec0001
MikeReh <59660736+MikeReh@users.noreply.github.com>
2020-05-25 12:58:50 +0200

    rename image

--- docker-compose/platform/clean.sh
+++ docker-compose/platform/clean.sh
@@ -6,7 +6,7 @@ rm -rf pgdata
 
 version=P-v1.8.1
 
-docker image rm liveobjects/install-postgres:$version
+docker image rm liveobjects/postgres:$version
 docker image rm liveobjects/service:$version
 docker image rm liveobjects/learn:$version
 docker image rm liveobjects/ui:$version
